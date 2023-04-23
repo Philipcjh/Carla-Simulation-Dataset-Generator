2dfd98a39110559323a5a4e5db165ac5605c0a3e
philip <13427291138@163.com>
2023-04-24 00:09:01 +0800

    update

--- label_utils.py
+++ label_utils.py
@@ -37,7 +37,7 @@ def spawn_dataset(data):
         intrinsic = dataDict["intrinsic"]
         extrinsic = dataDict["extrinsic"]
         sensors_data = dataDict["sensor_data"]
-        sensor_transform = dataDict["transform"]
+        # sensor_transform = dataDict["transform"]
 
         image_labels_kitti = []
         pc_labels_kitti = []
